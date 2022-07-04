6e1e8c4d1db281639ab23342fce4fe9a680ac6a4
Juanma <juan_manuel_aguirre1983@hotmail.com>
2022-07-04 10:42:29 +0200

    paises app

---
+++ angular/03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html'
+})
+export class PorCapitalComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string): void {
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital(termino)
+      .subscribe( (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      }, (err) => {
+        this.hayError = true;
+        this.paises = [];
+      });
+  }
+
+  sugerencias(termino: string){
+    this.hayError = false;
+  }
+
+}
