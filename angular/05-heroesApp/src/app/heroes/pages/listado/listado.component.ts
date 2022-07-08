ed437bd1cbe17d97ed12ece8bf9ecc8f6db2f57f
Juanma <juan_manuel_aguirre1983@hotmail.com>
2022-07-08 12:51:02 +0200

    heroesApp

---
+++ angular/05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styles: [
+    
+  ]
+})
+export class ListadoComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+
+  constructor( private heroesService: HeroesService) { }
+
+  ngOnInit(): void {
+
+    this.heroesService.getHeroes()
+      .subscribe( heroes => { this.heroes = heroes; });
+
+  }
+
+}
