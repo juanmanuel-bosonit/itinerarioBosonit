1a3329eb2e4818475a87ed5965c22e56b2876181
Juanma <juan_manuel_aguirre1983@hotmail.com>
2022-07-13 15:33:42 +0200

    Fin de aplicación

--- angular/05-heroesApp/src/app/heroes/pages/home/home.component.ts
+++ angular/05-heroesApp/src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
+
+type NewType = AuthService;
 
 @Component({
   selector: 'app-home',
@@ -13,9 +18,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  get auth() {
+    return this.authService.auth;
+  }
+
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.router.navigate(['/auth']);
   }
 
 }
