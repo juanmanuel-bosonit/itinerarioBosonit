f66d61d70c45feec58ef37cb8feff64d4d1cd5bc
Juanma <juan_manuel_aguirre1983@hotmail.com>
2022-07-18 21:09:06 +0200

    formularios terminados

---
+++ angular/06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import { emailPattern, nombreApellidoPattern, noPuedeSerIrathor } from 'src/app/shared/validator/validaciones';
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styles: [
+  ]
+})
+export class RegistroComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [ Validators.required, Validators.pattern(this.validatorService.nombreApellidoPattern) ] ],
+    email: [ '', [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ], [ this.emailValidator ] ],
+    userName: [ '', [ Validators.required, this.validatorService.noPuedeSerIrathor ] ],
+    password: [ '', [ Validators.required, Validators.minLength(6) ] ],
+    password2: [ '', [ Validators.required ] ]
+  }, {
+    validators: [ this.validatorService.camposIguales('password', 'password2') ]
+  });
+
+  get emailErrorMsj(): string {
+
+    const errors = this.miFormulario.get('email')?.errors;
+
+    if(errors?.['required']){
+      return 'Email es obligatorio.';
+    } else if(errors?.['pattern']) {
+      return 'El Email introducido no tiene formato de correo.';
+    } else if(errors?.['emailTomado']) {
+      return 'El Email ya está en uso.';
+    }
+
+    return '';
+
+  }
+
+  constructor(private fb: FormBuilder,
+              private validatorService: ValidatorService,
+              private emailValidator: EmailValidatorService) { }
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Juanma Aguirre',
+      email: 'test1@test.com',
+      userName: 'user1',
+      password: '123456',
+      password2: '123456'
+    })
+  }
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid
+           && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
+  }
+
+}
--- angular/06-formulariosApp/src/app/shared/sidemenu/sidemenu.component.ts
+++ angular/06-formulariosApp/src/app/shared/sidemenu/sidemenu.component.ts
@@ -46,4 +46,15 @@ export class SidemenuComponent {
     }
   ];
 
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    },
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    }
+  ]
+
 }
