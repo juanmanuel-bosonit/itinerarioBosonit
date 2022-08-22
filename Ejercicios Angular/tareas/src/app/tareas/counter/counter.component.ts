e51c625338a129f6bc5144840313bd47fe2ef770
Juanma <juan_manuel_aguirre1983@hotmail.com>
2022-08-22 09:56:09 +0200

    Counter finalizado

--- Ejercicios Angular/tareas/src/app/tareas/counter/counter.component.ts
+++ Ejercicios Angular/tareas/src/app/tareas/counter/counter.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styles: [
-  ]
+  styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
 
-  constructor() { }
+  count: number = 0;
+  step: number = 1;
+  up: boolean = true;
+  
+  public intervalTimer = interval(1000);
+  private _unsubscribe$ = new Subject<boolean>();
 
-  ngOnInit(): void {
+  start() {
+
+    this.intervalTimer
+    .pipe(
+      takeUntil( this._unsubscribe$ )
+    )
+    .subscribe(
+      () => this.up ? this.count += this.step : this.count -= this.step
+    ) 
+
   }
+
+  pause() {
+    this._unsubscribe$.next(false);
+  }
+
+  reset() {
+    this.count = 0;
+    this.step = 1;
+  }
+
+  countUp() {
+    this.up = true;
+  }
+
+  countDown() {
+    this.up = false;
+  }
+
+  onChangeSet( event: any ) {
+    console.log(event)
+    this.count = parseInt( event.target.value );
+  }
+
+  onChangeStep( event: any ) {
+    this.step = parseInt( event.target.value );
+  }
+
 
 }
